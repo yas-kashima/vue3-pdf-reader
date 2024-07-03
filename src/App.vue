09cd8e03cd80d919ef185bdca9d8bce69a5b6765
yas-kashima <yas.blizzard@gmail.com>
2024-07-03 13:53:47 +0900

    fix router

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/pdf-viewer">PDF</RouterLink>
       </nav>
     </div>
   </header>
